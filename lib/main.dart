61ebe5f4b20f9144fcf1cdb0c5d0720b1132fbb2
Toàn Đào Minh <toandaominh1997@gmail.com>
2019-01-14 00:46:31 +0700

    Add Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,128 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
+import 'package:logy_app/componets/horizontal_listview.dart';
+import 'package:shopapp_tut/componets/products.dart';
+void main(){
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    )
+  );
+}
+class HomePage extends StatefulWidget{
+  @override
+  HomePageState createState()=> HomePageState();
 
-void main() => runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class HomePageState extends State<HomePage>{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    // TODO: implement build
+    Widget image_carousel = new Container(
+      height: 200.0,
+      child:  new Carousel(
+        boxFit: BoxFit.cover,
+        images: [
+          AssetImage('images/w3.jpeg'),
+          AssetImage('images/m1.jpeg'),
+          AssetImage('images/c1.jpg'),
+          AssetImage('images/w4.jpeg'),
+          AssetImage('images/m2.jpg'),
+        ],
+        autoplay: false,
+        //      animationCurve: Curves.fastOutSlowIn,
+        animationDuration: Duration(milliseconds: 1000),
+        dotSize: 4.0,
+        indicatorBgPadding: 2.0,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: new AppBar(
+        backgroundColor: Colors.red,
+        title: Text("Logy"),
+        actions:<Widget>[
+          new IconButton(icon: Icon(Icons.search, color: Colors.white,), onPressed: null),
+          new IconButton(icon: Icon(Icons.shopping_cart, color: Colors.white,), onPressed: null)
+        ]
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      
+      drawer: new Drawer(
+        child: new ListView(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            new UserAccountsDrawerHeader(
+                accountName: Text("Bigkizd"),
+                accountEmail: Text("toandaominh1997@gmail.com"),
+                currentAccountPicture: GestureDetector(
+                  child: new CircleAvatar(
+                    backgroundColor: Colors.grey,
+                    child: Icon(Icons.person, color: Colors.white,),
+                  ),
+                ),
+              decoration: new BoxDecoration(
+                color: Colors.red
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            InkWell(
+                child: ListTile(
+                  title: Text("Home"),
+                  leading: Icon(Icons.home),
+                )
+            ),
+            InkWell(
+                child: ListTile(
+                  title: Text("My account"),
+                  leading: Icon(Icons.person),
+                )
+            ),
+            InkWell(
+                child: ListTile(
+                  title: Text("My orders"),
+                  leading: Icon(Icons.shopping_basket),
+                )
+            ),
+            InkWell(
+                child: ListTile(
+                  title: Text("Categories"),
+                  leading: Icon(Icons.dashboard),
+                )
+            ),
+            InkWell(
+                child: ListTile(
+                  title: Text("Favorites"),
+                  leading: Icon(Icons.favorite),
+                )
+            ),
+            Divider(),
+            InkWell(
+                child: ListTile(
+                  title: Text("Settings"),
+                  leading: Icon(Icons.settings, color: Colors.blue),
+                )
+            ),
+            InkWell(
+                child: ListTile(
+                  title: Text("About"),
+                  leading: Icon(Icons.help, color: Colors.green,),
+                )
             ),
           ],
-        ),
+        )
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: new ListView(
+        children: <Widget>[
+          image_carousel,
+          //padding widget
+          new Padding(padding: const EdgeInsets.all(8.0),
+            child: new Text('Categories'),),
+
+          //Horizontal list view begins here
+          HorizontalList(),
+        ],
+      )
     );
+    return null;
   }
+
 }
